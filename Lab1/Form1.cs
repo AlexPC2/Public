7b4c9718be042b792e1c031d1f202487c0f18cef
Alex Noyanov <alex.noyanov@gmail.com>
2019-02-05 23:58:18 +0300

    Added some comments about the algorithm

--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,23 +16,24 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)              // Кнопка нажата
+        
         {
            // String analog = textBox1.Text;
 
 
-            string a = textBox1.Text;
-            var c = Encoding.Default.GetChars(Encoding.Default.GetBytes(a)); //получаю массив char[]
+            string a = textBox1.Text;                                        // Ввод строки
+            var c = Encoding.Default.GetChars(Encoding.Default.GetBytes(a)); // Получаю массив char[]
 
-            char[] noDouble = c.Distinct().ToArray();
-            double countOfBite = Math.Log(noDouble.Length, 2);
-            String result = null;
+            char[] noDouble = c.Distinct().ToArray();                        // Массив из уникальных букв (Алфавит)
+            double countOfBite = Math.Log(noDouble.Length, 2);               // Требуемое число бит для кодировки слова
+            String result = null;                                            // Результат - переведенная строка
      
-            for (int i = 0; i < c.Length; i++)
+            for (int i = 0; i < c.Length; i++)                               // Перебор всех букв исходной строки
             {
-                for(int j = 0; j < noDouble.Length; j++)
+                for(int j = 0; j < noDouble.Length; j++)                     // Каждую букву исходной строки сравниваем с алфавитом
                 {
-                    if (c[i] == noDouble[j])
+                    if (c[i] == noDouble[j])                                 // Находим номер символа в алфавите
                     {/*
                         String strNull = "";
                         for (int n = 0; n < Convert.ToInt32(countOfBite) - Convert.ToInt32(Math.Log(j+1,2) - 1); n++ )
@@ -42,12 +43,12 @@ namespace WindowsFormsApplication1
                         }
                         result =   result strNull+ Convert.ToString(j, 2) + " ";
                         */
-                        result =  result + Convert.ToString(j, 2) + " ";
+                        result =  result + Convert.ToString(j, 2) + " ";      // К строке результата добавляем новый символ по номеру исходного в алфавите
                     }
                 }
                 
             }
-            textBox2.Text = result;
+            textBox2.Text = result;                                           // Вывод результата в строке вывода
          // textBox2.Text = Convert.ToString(noDouble.Length);  //конвертирую обратно
            
 

254b93d288af061403fb25fceb27b286596435a0
Alex Noyanov <alex.noyanov@gmail.com>
2019-02-12 20:50:55 +0300

    Lab2 #1 modified and all formulas added. Unfortunately the results are not correct at all!

--- Lab2-1/Lab2-1/main.cpp
+++ Lab2-1/Lab2-1/main.cpp
@@ -25,14 +25,55 @@
  
  1. Проверить формулы.
  
+ 2. Составить программу, которая вычисляет разложение функции f(x) для различных m определить  максимальную разность между значениями функции f(x) и c помощью  разложения Фурье.
+ 3.  Построить график при различных m  сравнить визуально схожесть графиков исходной функции и разложения Фурье.
+
+                       n
+                      ____
+            a0       \    |
+ g(t) =    ____  +    \      [am* cos(mx) + bm*sin(mx)]
+            2         /
+                     /____|
+                     m = 1
+ 
+ F(g(t)) =    A/(pi*f) * sin(pi+T)
+ 
  */
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+double b(double m)
+{
+    double bm = (pow(-1,m)+1)/(M_PI * m);
+    return bm;
+}
+
+double g(int  x)
+{
+    double result = 0;
+    double sum = 0;
+    
+    for(int i = 0; i < x ; i++)
+    {
+        cout << "B(" << i << ")" << "=" << b(i) << endl;
+        sum = sum + b(i) * sin(i);
+    }
+    result = 1.0/2.0 + sum;
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     //std::cout << "Hello, World!\n";
+    
+    double yourValue;
+    
+    cout << ">";
+    cin >> yourValue;
+    cout << "Result:" << g(yourValue) << endl;
+    
     return 0;
 }
